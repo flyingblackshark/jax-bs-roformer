554fef981b927b2cffadc47158ecfc293eba35b8
FBS <aliu2000@outlook.com>
2024-12-17 15:26:47 +0800

    fix scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
-wget https://huggingface.co/anvuew/deverb_bs_roformer/resolve/main/deverb_bs_roformer_8_256dim_8depth.ckpt
--- install_gpu.sh
+++ install_gpu.sh
@@ -1,4 +1,3 @@
 pip install -U "jax[cuda12]"
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
-wget https://huggingface.co/anvuew/deverb_bs_roformer/resolve/main/deverb_bs_roformer_8_256dim_8depth.ckpt
